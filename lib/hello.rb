81003636ea1e107d1a94a9799ea8ba71dfb510e0
Paul K <pkfis@outlook.com>
2019-04-06 07:11:40 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,19 @@
 def hello_t(array)
-  i = 0
-  while i < array.length
-    yield array[i]
-    i = i + 1
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+
+    array
+  else
+    puts "Hey! No block was given!"
   end
-  array
 end
 
 # call your method here!
-hello_t(array).each do |name|
-  puts name
-end
+#hello_t(array).each do |name|
+#  puts name
+#end
